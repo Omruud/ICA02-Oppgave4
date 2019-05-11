f34a3c15abed16abcc1755f72ed8d4cdc8cb5976
Ole Martin <einnynorskentusiast@gmail.com>
2019-05-11 14:52:13 +0200

    Dette er koden til oppgave 4b.go ase enter the commit message for your changes. Lines starting

--- 4b.go
+++ 4b.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-      "fmt"
+	"fmt"
 )
 
-
 // Kode for Oppgave 4b
-func UnicodeCodePointDemo() {
+func main() {
 	// Hva er dette?
 	// Er det likt på MS Windows og macOS?
-  fmt.Println("\xf0\x9F\x98\x80")
-  fmt.Println("\xf0\x9F\x98\x97")
-  // Demonstrerer at deler av et tegn representert med flere bytes
-  // kan ikke tolkes innenfor koden (unicode)
-  fmt.Println("\xf0\x9F\x98")
-  fmt.Println("\xf0\x9F")
-  fmt.Println("\xf0")
+	fmt.Println("\xf0\x9F\x98\x80")
+	fmt.Println("\xf0\x9F\x98\x97")
+	// Demonstrerer at deler av et tegn representert med flere bytes
+	// kan ikke tolkes innenfor koden (unicode)
+	fmt.Println("\xf0\x9F\x98")
+	fmt.Println("\xf0\x9F")
+	fmt.Println("\xf0")
 }
